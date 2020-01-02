edd651a9b41dcb424bf21404a86112afc4f5be02
Tim Coppinger <tim.coppinger@auden.co.uk>
2020-01-02 16:29:06 +0000

    Initial commit.

---
+++ DayOne/Services/FuelCalculator.cs
@@ -0,0 +1,11 @@
+namespace DayOne.Services
+{
+    public class FuelCalculator
+    {
+     public int getFuelRequired(int mass)
+     {
+     int fuelRequired = 0;
+     return fuelRequired;     
+     }
+    }
+}
